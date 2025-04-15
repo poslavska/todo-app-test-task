3790e83a58341a2a2b18d75e97c1a4c7a2956d29
poslavska <lyuda.posl@gmail.com>
2025-04-15 15:09:23 +0300

    Refactor the logic using filter func

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,13 +1,11 @@
 import { TodoType } from '../components/Todo'
 
 export function countCompletedTasks(data: TodoType[]) {
-  let count = 0
-  const newArr = data.map((todo) => (todo.completed ? count++ : count))
-  return count
+  const tasksTotal = data.filter(todo => todo.completed).length
+  return tasksTotal
 }
 
 export function countUncompletedTasks(data: TodoType[]) {
-  let count = 0
-  const newArr = data.map((todo) => (!todo.completed ? count++ : count))
-  return count
+  const tasksTotal = data.filter(todo => !todo.completed).length
+  return tasksTotal
 }
